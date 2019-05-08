d835101adc774f12ab866aecb6600d147a937c5e
Jakub <tagisgame@gmail.com>
2019-05-08 10:50:29 +0200

    Zad1 działa poprawnie

--- zad1.js
+++ zad1.js
@@ -1,1 +1,14 @@
-test
+//Final variable
+var result = "";
+
+//Input
+[,,num] = process.argv; //Number given by user
+
+//Program
+for(let i = 1; i <= num; i++){
+  if(i%2)
+    result += i + "; ";
+}
+
+//Print result
+console.log(result)
