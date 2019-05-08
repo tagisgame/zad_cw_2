98331971f2be6939ed0863a0b2a80f3ec8d088ea
Jakub <tagisgame@gmail.com>
2019-05-08 13:31:40 +0200

    Zadanie 7 z poprawionymi (?) komentarzami?

--- zad7.js
+++ zad7.js
@@ -1,33 +1,42 @@
-//Includowanie readline-sync do skryptu
-  const readlineSync = require('readline-sync');
+// #region Includowanie readline-sync do skryptu
 
-//Zmienna finalnie wyświetlana jako wynik działania programu
-  var result = "";
+const readlineSync = require('readline-sync');
 
-//Pobieranie wartości od użytkownika
-  var char = readlineSync.keyIn("Podaj znak, który ma być wypełnieniem prostokąta: ");
-  var startx = readlineSync.questionInt("Podaj x lewego górnego narożnika: ");
-  var starty = readlineSync.questionInt("Podaj y lewego górnego narożnika: ");
-  var w = readlineSync.questionInt("Podaj szerokość prostokąta: ");
-  var h = readlineSync.questionInt("Podaj wysokość prostokąta: ");
+// #endregion
+// #region Zmienna finalnie wyświetlana jako wynik działania programu
 
-//Operacje wykonywane przez program
-  //ustawienie początkowe y
-  for(let i = 0; i < starty; i++)
-    result += "\n";
+var result = "";
 
-  //rysowanie prostokąta
-  for(let y = starty; y < starty + h; y++){
-    for(let x = 0; x < (startx + w); x++){
-      if(x < startx)
-        result += " ";
-      else
-        result += char;
-    }
-    result += "\n";
+// #endregion
+// #region Pobieranie wartości od użytkownika
+
+var char = readlineSync.keyIn("Podaj znak, który ma być wypełnieniem prostokąta: ");
+var startx = readlineSync.questionInt("Podaj x lewego górnego narożnika: ");
+var starty = readlineSync.questionInt("Podaj y lewego górnego narożnika: ");
+var w = readlineSync.questionInt("Podaj szerokość prostokąta: ");
+var h = readlineSync.questionInt("Podaj wysokość prostokąta: ");
+
+// #endregion
+// #region Operacje wykonywane przez program
+
+//ustawienie początkowe y
+for(let i = 0; i < starty; i++)
+  result += "\n";
+
+//rysowanie prostokąta
+for(let y = starty; y < starty + h; y++){
+  for(let x = 0; x < (startx + w); x++){
+    if(x < startx)
+      result += " ";
+    else
+      result += char;
   }
+  result += "\n";
+}
 
-//Konstrukcja wyniku działania
+// #endregion
+// #region Wyświetlanie wyniku działania
 
-//Wyświetlanie wyniku działania
     console.log(result)
+
+// #endregion
