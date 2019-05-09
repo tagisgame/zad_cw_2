0368aaa65cc085273c689ea515b20ceaf8c1eab0
Jakub <tagisgame@gmail.com>
2019-05-09 09:54:59 +0200

    Zadanie ukończone

--- zad6.js
+++ zad6.js
@@ -1,23 +1,27 @@
-//Includowanie readline-sync do skryptu
-  const readlineSync = require('readline-sync');
+// <editor-fold desc="Includowanie readline-sync">
+const readlineSync = require('readline-sync');
+// </editor-fold>
 
 //Zmienna finalnie wyświetlana jako wynik działania programu
-  var result = "";
+var result = "";
 
-//Przygotowanie programu
-  var rand = Math.floor(Math.random() * 100);
+// <editor-fold desc="Operacje wykonywane przez program">
+//Przygotowanie losowej liczby
+var rand = Math.floor(Math.random() * 100);
 
 //Część właściwa programu
-  var guess = -1; //strzał gracza
-  do{
-    guess = readlineSync.questionInt("Podaj swój strzał: ");
-    if(guess > rand)
-      console.log("Podałeś za dużą wartość");
-    else if(guess < rand)
-      console.log("Podałeś za małą wartość");
-    else
-      result = "Gratulacje";
-  } while(guess != rand)
+while(true){
+  let guess = readlineSync.questionInt("Podaj swój strzał: ");
+  if(guess > rand)
+    console.log("Podałeś za dużą wartość");
+  else if(guess < rand)
+    console.log("Podałeś za małą wartość");
+  else{
+    result = "Gratulacje";
+    break;
+  }
+}
+// </editor-fold>
 
 //Wyświetlanie wyniku działania
-  console.log(result)
+  console.log(result);
